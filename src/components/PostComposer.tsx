99f3bee0aca1ada5aff20773687350b1f9c91868
ratheechakshita418 <ratheechakshita418@gmail.com>
2026-07-23 14:19:03 +0530

    Refactor PostComposer to use Redux for post state

--- src/components/PostComposer.tsx
+++ src/components/PostComposer.tsx
@@ -1,11 +1,16 @@
 import "../styles/PostComposer.css";
 
-interface Props {
-  post: string;
-  setPost: (value: string) => void;
-}
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 
-const PostComposer = ({ post, setPost }: Props) => {
+import { updatePost } from "../features/posts/postSlice";
+
+const PostComposer = () => {
+  const dispatch = useAppDispatch();
+
+  const post = useAppSelector(
+    (state) => state.posts.currentPost
+  );
+
   return (
     <div className="post-composer">
       <h2>Compose Your Post</h2>
@@ -13,7 +18,7 @@ const PostComposer = ({ post, setPost }: Props) => {
       <textarea
         placeholder="Write your social media post here..."
         value={post}
-        onChange={(e) => setPost(e.target.value)}
+        onChange={(e) => dispatch(updatePost(e.target.value))}
       />
     </div>
   );
