6aa364a2f5650d4f98e70a9932c5b1466b38a85b
ratheechakshita418 <ratheechakshita418@gmail.com>
2026-07-23 14:12:22 +0530

    Create platformSlice for managing selected platforms

---
+++ src/features/platforms/platformSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { PlatformState } from "./platformTypes";
+
+const initialState: PlatformState = {
+  selectedPlatforms: [],
+};
+
+const platformSlice = createSlice({
+  name: "platforms",
+
+  initialState,
+
+  reducers: {
+    togglePlatform(state, action: PayloadAction<string>) {
+      const platform = action.payload;
+
+      if (state.selectedPlatforms.includes(platform)) {
+        state.selectedPlatforms = state.selectedPlatforms.filter(
+          (item) => item !== platform
+        );
+      } else {
+        state.selectedPlatforms.push(platform);
+      }
+    },
+
+    clearPlatforms(state) {
+      state.selectedPlatforms = [];
+    },
+  },
+});
+
+export const {
+  togglePlatform,
+  clearPlatforms,
+} = platformSlice.actions;
+
+export default platformSlice.reducer;
